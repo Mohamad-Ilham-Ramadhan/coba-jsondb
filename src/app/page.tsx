0df7afabc401ec3614aa0e67eae5636f29966604
Mohamad-Ilham-Ramadhan <mohamadilhamramadhan@gmail.com>
2024-11-04 13:04:06 +0700

    add public image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,6 +26,10 @@ export default function Home() {
           <li>Mohamad Ilham Ramadhan - was here.</li>
         </ol>
 
+        <div>
+          <img src="/images/kuda-burung.jpeg" alt="dusha" />
+        </div>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
